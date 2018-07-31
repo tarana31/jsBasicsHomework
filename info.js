68100e372a14ce63dab01b8018cbd284a1c77aa6
Tarana Patel <tarana3121@gmail.com>
2018-07-31 17:57:27 -0400

    init commit

---
+++ info.js
@@ -0,0 +1,84 @@
+//Problem: 1
+
+var num1= 5;
+var num2= 10;
+var total= num1+num2;
+console.log(total);
+
+// var str1= "hello";
+// var str2= "world";
+// var sum= (str1 +" "+ str2);
+// console.log(sum);
+
+
+//Problem: 2
+// var sub1= ["html","css","wikdei"];
+// var sub2= ["Java","php","ruby"];
+// var subject= [sub1,sub2];
+// console.log(subject[1][1]);
+// console.log(subject[0][2]);
+
+
+//Problem: 3
+
+// var numOne= prompt();
+// if(numOne<=100){
+//     alert("Entered variable less than 100")
+// }
+// else{
+//     alert(numOne + " is greater than 100")
+// }
+
+//Problem: 4
+
+// var numOne= prompt();
+// if(numOne<=100){
+//     alert("Entered variable less than 100")
+// }
+// else{
+//     alert(numOne + " is greater than 100")
+// }
+
+
+//Problem: 5
+
+// var str3 = prompt();
+// if(str3== "goodluck"){
+//     alert("Your word is same")
+// }else{
+//     alert("your word is not same")
+// }
+
+
+//Problem: 6
+
+// function person(fname){    
+//     return alert(fname);
+// }
+// person("Tarana");
+
+
+//Problem: 7
+
+// function something(){
+//     console.log(4);
+// }
+// something();
+
+
+//Problem: 8
+
+// function final()
+//     if(door==1){
+//         alert("You have received the $100 prize money")
+//     }
+//     else if(door==2){
+//         alert("You have won the prize money")
+//     }
+//     else if (door==3){
+//         alert("You have won won the Jackpot")
+//     }
+//     else{
+//         alert("Bad Luck..Try again")
+//     }
+// final(2);
